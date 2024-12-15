d78342b7e26ed0308f14cee1df7a015bae05d140
hariwr <hariwr25@gmail.com>
2024-12-15 23:17:20 +0700

    Menambahkan class HomeMhsViewModel

---
+++ app/src/main/java/com/example/praktikum7/ui/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.praktikum7.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.praktikum7.repository.RepositoryMhs
+
+class HomeMhsViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+}

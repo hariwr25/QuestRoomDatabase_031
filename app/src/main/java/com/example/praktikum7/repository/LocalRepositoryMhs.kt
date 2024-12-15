063b1fdeaf96ba3845647c2e97f864a712619c57
hariwr <hariwr25@gmail.com>
2024-12-15 23:02:41 +0700

    Menambahkan function getAllmhs, getMhs, deleteMhs, dan update Mhs pada LocalRepositoryMhs

--- app/src/main/java/com/example/praktikum7/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/praktikum7/repository/LocalRepositoryMhs.kt
@@ -2,12 +2,24 @@ package com.example.praktikum7.repository
 
 import com.example.praktikum7.data.dao.MahasiswaDao
 import com.example.praktikum7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
-class LocalRepositoryMhs(
+class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs {
-
-    override suspend fun insertMhs(mahasiswa: Mahasiswa){
+    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+    override fun getAllMhs(): Flow<List<Mahasiswa>> =
+        mahasiswaDao.getAllMahasiswa()
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> =
+        mahasiswaDao.getMahasiswa(nim)
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }

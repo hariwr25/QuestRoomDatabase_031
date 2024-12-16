e3c9ebeb66b477bf6290d86d64d8db71b62c4f50
hariwr <hariwr25@gmail.com>
2024-12-16 12:34:58 +0700

    Menambahkan val detailUiState

--- app/src/main/java/com/example/praktikum7/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum7/ui/viewmodel/DetailMhsViewModel.kt
@@ -2,12 +2,43 @@ package com.example.praktikum7.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.praktikum7.repository.RepositoryMhs
 import com.example.praktikum7.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
-class DetailMhsViewModel (
+class DetailMhsViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs,
 ) : ViewModel() {
     private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                dataUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+    }
+        .onStart {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = true,
+                ),
+    )
 }

fd8e4560426c5feb32d19ee48d8d8a786e0d33b8
hariwr <hariwr25@gmail.com>
2024-12-17 01:17:22 +0700

    Fix Error dan membuat function HomeMhsView

--- app/src/main/java/com/example/praktikum7/ui/View/mahasiswa/insertMhsView.kt
+++ app/src/main/java/com/example/praktikum7/ui/View/mahasiswa/insertMhsView.kt
@@ -1,7 +1,6 @@
 package com.example.praktikum7.ui.View.mahasiswa
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,7 +15,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -24,12 +22,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.praktikum7.data.entity.Mahasiswa
 import com.example.praktikum7.ui.costumwidget.TopAppBar
 import com.example.praktikum7.ui.navigation.Alamatnavigasi
 import com.example.praktikum7.ui.viewmodel.FormErrorState
@@ -78,7 +73,9 @@ fun InsertMhsView(
             TopAppBar(
                 onBack = onBack,
                 showBackButton = true,
-                judul = "Tambah Mahasiswa")
+                judul = "Tambah Mahasiswa",
+                modifier = modifier
+            )
             InsertBodyMhs(
                 uiState = uiState,
                 onValueChange = {updatedEvent ->

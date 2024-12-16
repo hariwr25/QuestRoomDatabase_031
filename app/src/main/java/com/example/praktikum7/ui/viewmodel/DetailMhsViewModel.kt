571224889a3dba7dc38a65d7ba3d67b5c5bb74a2
hariwr <hariwr25@gmail.com>
2024-12-16 12:42:24 +0700

    Menambahkan data class DetailUiState

--- app/src/main/java/com/example/praktikum7/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum7/ui/viewmodel/DetailMhsViewModel.kt
@@ -52,3 +52,16 @@ class DetailMhsViewModel(
         }
     }
 }
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}

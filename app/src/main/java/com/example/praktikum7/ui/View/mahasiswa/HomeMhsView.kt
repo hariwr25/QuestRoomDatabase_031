0905ad6fb9b474100967d1a424d0e3360c378c61
hariwr <hariwr25@gmail.com>
2024-12-17 01:06:40 +0700

    Membuat function CardMhs pada HomeMhsView.kt

---
+++ app/src/main/java/com/example/praktikum7/ui/View/mahasiswa/HomeMhsView.kt
@@ -0,0 +1,14 @@
+package com.example.praktikum7.ui.View.mahasiswa
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.praktikum7.data.entity.Mahasiswa
+
+@Composable
+fun CardMhs (
+    mhs : Mahasiswa,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+) {
+
+}
